71c80b6fff5d0c8e0e0c1f66a1ec1de9e807ec9b
Aman <amankumar.edith@gmail.com>
2020-01-22 21:56:36 +0530

    Implemented api for subscribing to store

--- index.js
+++ index.js
@@ -13,17 +13,43 @@ const createStore = () => {
   // state of the store
   let state;
 
+  // listeners(callback ()s) to invoke on state change
+  let listeners = [];
+
   // returns the state of the store
   const getState = () => {
     return state;
   };
 
+  // subsribes a callback for state change
+  const subscribe = listener => {
+    listeners.push(listener);
+
+    // for testing purposes only
+    console.log(listeners.length);
+
+    // api to unsubscribe from store
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+      console.log(listeners.length);
+    };
+  };
+
   return {
-    getState
+    getState,
+    subscribe
   };
 };
 
 const store = createStore();
 
-console.log(typeof store); // object
-console.log(store.getState()); // undefined
+unsubsribe_1 = store.subscribe(() =>
+  console.log("Store state changed: ", store.getState)
+); // 1
+
+unsubsribe_2 = store.subscribe(() =>
+  console.log(`Store state changed: ${store.getState()}`)
+); // 2
+
+unsubsribe_1(); // 1
+unsubsribe_2(); // 0
